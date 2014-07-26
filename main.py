9c81b2f06fa273b33c8b563090b966d294290091
nava <navaneethanit@gmail.com>
2014-07-26 11:39:13 +0530

    recording sample html into file

--- main.py
+++ main.py
@@ -7,14 +7,17 @@ import time
 
 url = "http://www.flipkart.com/search?q=%s&as=off&as-show=on&otracker=start"
 driver = webdriver.Firefox()
+fo = open('out.txt', 'w')
+
 
 def close(signal, frame):
     global driver
     print('You pressed Ctrl+C!')
     driver.close()
     signal.pause()
+    fo.close()
     sys.exit(0)
- 
+    
 
 def get_page(url, keyword="moto g"):
     global driver
@@ -22,7 +25,8 @@ def get_page(url, keyword="moto g"):
     return driver.page_source
 
 def parser(input):
-    pass
+    print "html recorded"
+    print >> fo, 'data',input.encode('utf-8',errors='ignore')
 
 def start():
     html_data = get_page(url)
@@ -30,6 +34,8 @@ def start():
     time.sleep(20)
 
 if __name__ == '__main__':
-    signal.signal(signal.SIGINT, close)
-    start()
-  
+    try:
+        signal.signal(signal.SIGINT, close)
+        start()
+    finally:
+        driver.close()
