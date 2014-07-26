3089e013eff78182dfdee1adf6e13eb2d4784611
nava <navaneethanit@gmail.com>
2014-07-26 11:04:58 +0530

    ctrl-c signal handler added

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
+import signal
+import sys
+import time
+
 url = "http://www.flipkart.com/search?q=%s&as=off&as-show=on&otracker=start"
 driver = webdriver.Firefox()
 
-def get_page(url):
+def close(signal, frame):
     global driver
-    driver.get(url)
-    return d.page_source
+    print('You pressed Ctrl+C!')
+    driver.close()
+    sys.exit(0)
+    signal.pause()
+
+def get_page(url, keyword="moto g"):
+    global driver
+    driver.get(url % keyword)
+    return driver.page_source
 
 def parser(input):
     pass
@@ -15,7 +26,9 @@ def parser(input):
 def start():
     html_data = get_page(url)
     result = parser(html_data)
-
+    time.sleep(20)
 
 if __name__ == '__main__':
+    signal.signal(signal.SIGINT, close)
     start()
+  
