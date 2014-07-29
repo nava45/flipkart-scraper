ab94798ea436877a5e1963cceb42eade86d9f85a
nava <navaneethanit@gmail.com>
2014-07-29 11:44:31 +0530

    scroll down the page function added

--- config.py
+++ config.py
@@ -2,6 +2,8 @@
 
 URL_FORMAT = "http://www.flipkart.com/search?q=%s&as=off&as-show=on&otracker=start"
 
+SCROLL_TIMES = 3
+
 MONGO_STORAGE_INPUT_DICT = {
                     'name':'',
                     'price':'',
--- main.py
+++ main.py
@@ -1,37 +1,56 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-from config import URL_FORMAT
+from config import URL_FORMAT, SCROLL_TIMES
 from parser import FParser
 
 import signal
 import sys
 import time
 
-driver = webdriver.Firefox()
+driver = None
+
 
 def close(signal, frame):
+    '''
+    When you press Ctrl-C the browser closes
+    '''
     global driver
     print('You pressed Ctrl+C!')
     driver.close()
     signal.pause()
     sys.exit(0)
-    
 
-def get_page(url):
-    global driver
+    
+def scroll_down(driver):
+    '''
+    This helps you to scroll the search results page to load more results
+    '''
+    for i in range(SCROLL_TIMES):
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(0.3)
+
+
+def get_page(driver, url):
     driver.get(url)
     return driver.page_source
 
-def start(keyword):
-    html_data = get_page(URL_FORMAT % keyword)
+
+def press_the_button_2_crawl(driver, keyword):
+    """
+    Main function handles everything 
+    """
+    html_data = get_page(driver, URL_FORMAT % keyword)
+    scroll_down(driver)
     parser = FParser(html_data)
     parser.store() #store into db
     time.sleep(2)
 
+
 if __name__ == '__main__':
     try:
+        driver = webdriver.Firefox()
         signal.signal(signal.SIGINT, close)
-        start('moto g')
+        press_the_button_2_crawl(driver,'moto g')
     finally:
         driver.close()
