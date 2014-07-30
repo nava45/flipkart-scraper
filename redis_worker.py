53dff72f0b56272e8ca824263782491050ad6019
nava <navaneethanit@gmail.com>
2014-07-30 13:38:41 +0530

    redis worker added

---
+++ redis_worker.py
@@ -0,0 +1,14 @@
+from redis import Redis
+
+from config import REDIS_CRAWLER_KEY
+from main import crawler_machine
+
+redis = Redis()
+
+def work():
+    while 1:
+        msg = redis.lpop(REDIS_CRAWLER_KEY)
+        crawler_machine(msg)
+
+if __name__ == '__main__':
+    work()
